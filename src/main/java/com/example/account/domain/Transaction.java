4f779ac238e52b10ecd3d9ffa355258722c5f429
phc09188 <phc09188@gmail.com>
2022-09-21 09:46:54 +0900

    Initial commit

---
+++ src/main/java/com/example/account/domain/Transaction.java
@@ -0,0 +1,35 @@
+package com.example.account.domain;
+
+
+import com.example.account.type.TransactionResultType;
+import com.example.account.type.TransactionType;
+import lombok.*;
+
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Transaction extends  BaseEntity {
+
+
+    @Enumerated(EnumType.STRING)
+    private TransactionType transactionType;
+    @Enumerated(EnumType.STRING)
+    private TransactionResultType transactionResultType;
+
+    @ManyToOne
+    private Account account;
+    private Long amount;
+    private Long balanceSnapshot;
+
+    private String transactionId;
+    private LocalDateTime transactedAt;
+
+
+}
